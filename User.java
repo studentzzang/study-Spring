9d5ab4fc1079016d5465ac299ba714d446fac578
JES-STUDIO <studentzzang@gmail.com>
2024-08-22 16:03:20 +0900

    UserDAO
    before fixing

---
+++ User.java
@@ -0,0 +1,29 @@
+public class User {
+    String id;
+    String name;
+    String pw;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getPw(){
+        return pw;
+    }
+
+    public void SetId(String id){
+        this.id = id;
+    }
+
+    public void SetName(String name){
+        this.name = name;
+    }
+
+    public void SetPw(String pw){
+        this.pw = pw;
+    }
+}
---
+++ UserDAO.java
@@ -0,0 +1,64 @@
+import java.sql.*;
+
+public class UserDAO {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/spring", "root", "1234");
+
+        PreparedStatement ps = c.prepareStatement(  "insert into users(id,name,password) values(?,?,?)");
+
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPw());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User getUser(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost/spring", "root", "1234");
+        PreparedStatement ps = c.prepareStatement(  "select * from users where id=?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+
+        User user = new User();
+
+        user.SetId(rs.getString("id"));
+        user.SetName(rs.getString("name"));
+        user.SetPw(rs.getString("password"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        UserDAO dao = new UserDAO();
+
+        User user = new User();
+
+        user.SetId("cat999");
+        user.SetName("고먐미");
+        user.SetPw("kitty2134");
+
+        dao.add(user);
+
+        System.out.println(user.getName() + " 등록성공");
+
+        User user2 = dao.getUser("cat999");
+        System.out.println(user2.getName() + "조회 성공");
+        System.out.println("id : " + user2.getId());
+        System.out.println("password : " + user2.getPw());
+    }
+}
+
+
